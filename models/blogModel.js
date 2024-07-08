abdcfac56b6d18c9f895ea81d0072b85bd86fc07
cliff-codes <simplecodes2580@gmail.com>
2024-07-08 12:40:57 +0000

    favourite blog field added to userSchema

--- models/blogModel.js
+++ models/blogModel.js
@@ -15,6 +15,14 @@ const blogSchema = new mongoose.Schema({
         ref: 'User',
         required: true
     },
+    upVotes: {
+        type: Number,
+        default: 0
+    }, 
+    downVotes: {
+        type: Number,
+        default: 0
+    },  
     comments: [
         {
             type: mongoose.Schema.Types.ObjectId,
