3358f3b02eb8cfca984c60b275510e0dc7803ffd
cliff-codes <simplecodes2580@gmail.com>
2024-07-27 17:50:07 +0000

    check user login status route updated

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,11 +1,12 @@
 import express from "express";
-import { createUser, loginUser } from "../controllers/userController.js";
+import { createUser, loginStatus, loginUser } from "../controllers/userController.js";
 
 
 const router = express.Router();
 
 router.post('/register', createUser) 
 router.post('/login', loginUser) 
+router.get('/status', loginStatus)
 
 
 
